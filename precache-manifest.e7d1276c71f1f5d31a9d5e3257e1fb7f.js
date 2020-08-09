7eb697cfef360573f2bf5223b1f0539f5134ae23
nihalkza <abnielit@gmail.com>
2020-08-09 18:22:56 +0530

    Updates

---
+++ precache-manifest.e7d1276c71f1f5d31a9d5e3257e1fb7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d5c23cee3c6b9dc7b469ac009c2323",
+    "url": "/todoMatic/index.html"
+  },
+  {
+    "revision": "d1cecc8dcd767d447f08",
+    "url": "/todoMatic/static/css/main.254061bc.chunk.css"
+  },
+  {
+    "revision": "b911cbfcc327534d3543",
+    "url": "/todoMatic/static/js/2.ca152ec1.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todoMatic/static/js/2.ca152ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cecc8dcd767d447f08",
+    "url": "/todoMatic/static/js/main.1a81e3c5.chunk.js"
+  },
+  {
+    "revision": "5c0e0f4746e3e5ebe69b",
+    "url": "/todoMatic/static/js/runtime-main.ba269d74.js"
+  }
+]);
